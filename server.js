bbfd6555bb3f69590e1dc869d6690c949d896948
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-01-29 13:54:09 +0530

    Removing Mongoose Deprication warnings

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 
 const app = express();
+// Bodyparser
+app.use(express.json());
 
-// Bodyparser Middleware
-app.use(bodyParser.json());
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
