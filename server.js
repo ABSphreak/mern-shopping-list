1fa8bab4a0aa54e67010b0463453d0912c576b32
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-01-29 12:41:11 +0530

    Adding MongoDB Atlas Config

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
