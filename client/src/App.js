9d2f586b97d847f5fd425ad088e4ea8fdfc76976
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-01-29 17:14:11 +0530

    Adding List

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ShoppingList from "./components/ShoppingList";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,3 +3,37 @@ import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import uuid from "uuid";
 
+class ShoppingList extends Component {
+  state = {
+    items: [
+      { id: uuid(), name: "Eggs" },
+      { id: uuid(), name: "Milk" },
+      { id: uuid(), name: "Steak" },
+      { id: uuid(), name: "Water" }
+    ]
+  };
+
+  render() {
+    const { items } = this.state;
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name) {
+              this.setState(state => {
+                items: [...state.items, { id: uuid(), name }];
+              });
+            }
+          }}
+        >
+          Add Item
+        </Button>
+      </Container>
+    );
+  }
+}
+
+export default ShoppingList;
