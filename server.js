688f886054b9ef62fb882ed36610c8a2cd5450b2
ABSphreak <absphreak@outlook.com>
2020-02-03 22:20:26 +0530

    Setting Heroku-scripts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const items = require("./routes/api/items");
 
 const app = express();
@@ -19,6 +19,17 @@ mongoose
 // User Routes
 app.use("/api/items", items);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// PORT
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
